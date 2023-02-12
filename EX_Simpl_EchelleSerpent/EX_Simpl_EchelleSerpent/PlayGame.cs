6fe4767adf09e32e135a63ca29289a6abb5fe6f2
GianlucaPtrn <91669728+GianlucaPtrn@users.noreply.github.com>
2023-02-12 21:08:28 +0100

    add files

---
+++ EX_Simpl_EchelleSerpent/EX_Simpl_EchelleSerpent/PlayGame.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace EX_Simpl_EchelleSerpent
+{
+    class PlayGame
+    {
+        private int _nbrPtsJoueur;
+        private int[] _positionPion;
+        private string _iconPlayer;
+        private string _lastValue;
+
+        public int NbrPtsJoueur
+        {
+            get { return _nbrPtsJoueur; }
+            set { _nbrPtsJoueur = value; }
+        }
+
+        public int[] PositionPion
+        {
+            get { return _positionPion; }
+            set { _positionPion = value; }
+        }
+
+        public string IconPlayer
+        {
+            get { return _iconPlayer; }
+            set { _iconPlayer = value; }
+        }
+
+        public string LastValue
+        {
+            get { return _lastValue; }
+            set { _lastValue = value; }
+        }
+
+        public PlayGame()
+        {
+            LastValue = "1";
+            PositionPion = new int[] { 0, 0 };
+            IconPlayer = "X";
+            NbrPtsJoueur = 1;
+        }
+
+        public void TourPlateau(int numeroJoueur)
+        {
+            MainWindow Jeu = (EX_Simpl_EchelleSerpent.MainWindow)App.Current.MainWindow;
+            Random alea = new Random();
+            int de = alea.Next(1, 7);
+            Jeu.nameJoueur.Text = "Joueur" + numeroJoueur;
+            Jeu.infoDe.Text = "Dé : " + de;
+            int reste;
+            Jeu.BtnCase[PositionPion[0], PositionPion[1]].Content = LastValue;
+            Jeu.BtnCase[PositionPion[0], PositionPion[1]].Foreground = Brushes.Black;
+            NbrPtsJoueur += de;
+            reste = NbrPtsJoueur - 10 * (PositionPion[0] + 1);
+            if (reste < 0)
+            {
+                reste += 10;
+            }
+            else
+            {
+                PositionPion[0] += 1;
+            }
+            if (PositionPion[0] % 2 != 0)
+            {
+                PositionPion[1] = 9 - reste;
+            }
+            else
+            {
+                PositionPion[1] = reste;
+            }
+            if (PositionPion[0] <= 9)
+            {
+                LastValue = Jeu.BtnCase[PositionPion[0], PositionPion[1]].Content.ToString();
+                Jeu.BtnCase[PositionPion[0], PositionPion[1]].Content = IconPlayer;
+                Jeu.BtnCase[PositionPion[0], PositionPion[1]].Foreground = Brushes.Gold;
+            }
+            else
+            {
+                Jeu.nameJoueur.Text = "Fin !";
+                Jeu.BtnCase[9, 0].Content = IconPlayer;
+                Jeu.BtnCase[9, 0].Foreground = Brushes.Gold;
+                Jeu.btnJouer.IsEnabled = false;
+            }
+        }
+    }
+}
---
+++ EX_Simpl_EchelleSerpent/EX_Simpl_EchelleSerpent/StructureQuadri.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows;
+
+namespace EX_Simpl_EchelleSerpent
+{
+    class StructureQuadri
+    {
+        MainWindow Structure = (EX_Simpl_EchelleSerpent.MainWindow)App.Current.MainWindow;
+        public void Quadrillage()
+        {
+            Structure.Width = 800;
+            Structure.Height = 720;
+
+            Grid grdPlateau = new Grid();
+            RowDefinition[] rowdef = new RowDefinition[10];
+            ColumnDefinition[] colDef = new ColumnDefinition[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                rowdef[i] = new RowDefinition();
+                grdPlateau.RowDefinitions.Add(rowdef[i]);
+            }
+
+            for (int y = 0; y < 10; y++)
+            {
+                colDef[y] = new ColumnDefinition();
+                grdPlateau.ColumnDefinitions.Add(colDef[y]);
+            }
+
+            Grid.SetColumn(grdPlateau, 0);
+            Grid.SetRow(grdPlateau, 1);
+            Structure.grdMain.Children.Add(grdPlateau);
+
+            for (int x = 0; x < 10; x++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Structure.BtnCase[x, j] = new Button();
+                    if (x % 2 == 0)
+                    {
+                        int numberCase = (10 * x) + j + 1;
+                        Structure.BtnCase[x, j].Content = Convert.ToString(numberCase);
+
+                    }
+                    else
+                    {
+                        int numberCase = (10 * x) + 10 - j;
+                        Structure.BtnCase[x, j].Content = Convert.ToString(numberCase);
+                    }
+
+                    if (int.Parse((string)Structure.BtnCase[x, j].Content) % 2 == 0)
+                    {
+                        Structure.BtnCase[x, j].Background = Brushes.Blue;
+                    }
+                    else
+                    {
+                        Structure.BtnCase[x, j].Background = Brushes.Red;
+                    }
+
+                    Structure.BtnCase[x, j].HorizontalAlignment = HorizontalAlignment.Center;
+                    Structure.BtnCase[x, j].VerticalAlignment = VerticalAlignment.Center;
+                    Structure.BtnCase[x, j].Width = 60;
+                    Structure.BtnCase[x, j].Height = 60;
+
+                    Grid.SetRow(Structure.BtnCase[x, j], x);
+                    Grid.SetColumn(Structure.BtnCase[x, j], j);
+
+                    grdPlateau.Children.Add(Structure.BtnCase[x, j]);
+                }
+            }
+        }
+    }
+}
